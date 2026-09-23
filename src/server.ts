53a4e2ef694d121407e78a7f09d21279eb8aa4af
MissMadde <missmadelene@outlook.com>
2026-09-23 14:36:24 +0200

    add typescript to get/

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import type { Request, Response } from "express";
 import { transactions } from "./data.js";
 
 const app = express();
@@ -6,13 +7,17 @@ const port = 3000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
+
+
+app.get("/", (req: Request, res: Response) => {
   res.send("Internet Bank API is running");
 });
 
-app.get("/transactions", (req, res) => {
+
+app.get("/transactions", (req: Request, res: Response) => {
   res.json(transactions);
 });
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
